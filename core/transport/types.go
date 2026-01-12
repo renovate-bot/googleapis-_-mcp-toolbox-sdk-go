5b83315d11225dc52896b56a1a55e877013bc974
dishaprakash <57954147+dishaprakash@users.noreply.github.com>
2026-01-12 12:40:17 +0000

    chore: Add Transport interface and Toolbox Transport (#126)
    * chore: Add Transport interface and Toolbox Transport
    
    * increase code coverage
    
    * add tests for types
    
    * linter fix
    
    * add build tags
    
    * add static check
    
    * review changes
    
    * update header to 2026
    
    * fix test
    
    * fix tests
    
    * change tokensources to resolved headers
    
    * minor fix
    
    * final fixes

---
+++ core/transport/types.go
@@ -0,0 +1,167 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package transport
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// Schema for a tool parameter.
+type ParameterSchema struct {
+	Name                 string           `json:"name"`
+	Type                 string           `json:"type"`
+	Required             bool             `json:"required,omitempty"`
+	Description          string           `json:"description"`
+	AuthSources          []string         `json:"authSources,omitempty"`
+	Items                *ParameterSchema `json:"items,omitempty"`
+	AdditionalProperties any              `json:"additionalProperties,omitempty"`
+}
+
+// ValidateType is a helper for manual type checking.
+func (p *ParameterSchema) ValidateType(value any) error {
+	if value == nil {
+		if p.Required {
+			return fmt.Errorf("parameter '%s' is required but received a nil value", p.Name)
+		}
+		return nil
+	}
+
+	switch p.Type {
+	case "string":
+		if _, ok := value.(string); !ok {
+			return fmt.Errorf("parameter '%s' expects a string, but got %T", p.Name, value)
+		}
+	case "integer":
+		switch value.(type) {
+		case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		default:
+			return fmt.Errorf("parameter '%s' expects an integer, but got %T", p.Name, value)
+		}
+	case "float":
+		switch value.(type) {
+		case float32, float64:
+		default:
+			return fmt.Errorf("parameter '%s' expects an float, but got %T", p.Name, value)
+		}
+	case "boolean":
+		if _, ok := value.(bool); !ok {
+			return fmt.Errorf("parameter '%s' expects a boolean, but got %T", p.Name, value)
+		}
+	case "array":
+		v := reflect.ValueOf(value)
+		if v.Kind() != reflect.Slice && v.Kind() != reflect.Array {
+			return fmt.Errorf("parameter '%s' expects an array/slice, but got %T", p.Name, value)
+		}
+		for i := range v.Len() {
+			item := v.Index(i).Interface()
+
+			if err := p.Items.ValidateType(item); err != nil {
+				return fmt.Errorf("error in array '%s' at index %d: %w", p.Name, i, err)
+			}
+		}
+	case "object":
+		// First, check that the value is a map with string keys.
+		valMap, ok := value.(map[string]any)
+		if !ok {
+			return fmt.Errorf("parameter '%s' expects a map, but got %T", p.Name, value)
+		}
+
+		switch ap := p.AdditionalProperties.(type) {
+		// No validation required, allows any type
+		case bool:
+
+		// Validate type for each value in map
+		case *ParameterSchema:
+			// Raise error if the input is a nested map / array
+			if ap.Type == "object" || ap.Type == "array" {
+				return fmt.Errorf("invalid schema for object '%s': values cannot be of type '%s'", p.Name, ap.Type)
+			}
+			for key, val := range valMap {
+				if err := ap.ValidateType(val); err != nil {
+					return fmt.Errorf("error in object '%s' for key '%s': %w", p.Name, key, err)
+				}
+			}
+
+		default:
+			// This is a schema / manifest error.
+			return fmt.Errorf(
+				"invalid schema for parameter '%s': AdditionalProperties must be a boolean or a map[string]any, but got %T",
+				p.Name,
+				ap,
+			)
+		}
+	default:
+		return fmt.Errorf("unknown type '%s' in schema for parameter '%s'", p.Type, p.Name)
+	}
+	return nil
+}
+
+// ValidateDefinition checks if the schema itself is well-formed.
+func (p *ParameterSchema) ValidateDefinition() error {
+	if p.Type == "" {
+		return fmt.Errorf("schema validation failed for '%s': type is missing", p.Name)
+	}
+
+	switch p.Type {
+	case "array":
+		if p.Items == nil {
+			return fmt.Errorf("parameter '%s' is an array but is missing item type definition", p.Name)
+		}
+		// Recursively validate the nested schema's definition.
+		if err := p.Items.ValidateDefinition(); err != nil {
+			return err
+		}
+
+	case "object":
+		switch ap := p.AdditionalProperties.(type) {
+		case bool:
+			// Valid scenario
+		case *ParameterSchema:
+			if err := ap.ValidateDefinition(); err != nil {
+				return err
+			}
+		default:
+			// Any other type is an invalid schema definition.
+			return fmt.Errorf(
+				"invalid schema for parameter '%s': AdditionalProperties must be a boolean or a schema, but got %T",
+				p.Name,
+				ap,
+			)
+		}
+
+	case "string", "integer", "float", "boolean":
+		// No type-specific rules for these.
+		break
+
+	default:
+		return fmt.Errorf("unknown schema type '%s' for parameter '%s'", p.Type, p.Name)
+	}
+
+	return nil
+}
+
+// Schema for a tool.
+type ToolSchema struct {
+	Description  string            `json:"description"`
+	Parameters   []ParameterSchema `json:"parameters"`
+	AuthRequired []string          `json:"authRequired,omitempty"`
+}
+
+// Schema for the Toolbox manifest.
+type ManifestSchema struct {
+	ServerVersion string                `json:"serverVersion"`
+	Tools         map[string]ToolSchema `json:"tools"`
+}
---
+++ core/transport/types_test.go
@@ -0,0 +1,581 @@
+//go:build unit
+
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package transport
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+)
+
+// Tests ParameterSchema with type 'int'.
+func TestParameterSchemaInteger(t *testing.T) {
+
+	schema := ParameterSchema{
+		Name:        "param_name",
+		Type:        "integer",
+		Description: "integer parameter",
+	}
+
+	t.Run("Test int param", func(t *testing.T) {
+		value := 1
+
+		err := schema.ValidateType(value)
+
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	})
+	t.Run("Test int8 param", func(t *testing.T) {
+		var value int8 = 1
+
+		err := schema.ValidateType(value)
+
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	})
+	t.Run("Test int16 param", func(t *testing.T) {
+		var value int16 = 1
+
+		err := schema.ValidateType(value)
+
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	})
+	t.Run("Test int32 param", func(t *testing.T) {
+		var value int32 = 1
+
+		err := schema.ValidateType(value)
+
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	})
+	t.Run("Test int64 param", func(t *testing.T) {
+		var value int64 = 1
+
+		err := schema.ValidateType(value)
+
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	})
+	t.Run("Test uint param", func(t *testing.T) {
+		var value uint = 1
+
+		err := schema.ValidateType(value)
+
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	})
+	t.Run("Test uint8 param", func(t *testing.T) {
+		var value uint8 = 1
+
+		err := schema.ValidateType(value)
+
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	})
+	t.Run("Test uint16 param", func(t *testing.T) {
+		var value uint16 = 1
+
+		err := schema.ValidateType(value)
+
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	})
+	t.Run("Test uint32 param", func(t *testing.T) {
+		var value uint32 = 1
+
+		err := schema.ValidateType(value)
+
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	})
+	t.Run("Test uint64 param", func(t *testing.T) {
+		var value uint64 = 1
+
+		err := schema.ValidateType(value)
+
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	})
+
+}
+
+// Tests ParameterSchema with type 'string'.
+func TestParameterSchemaString(t *testing.T) {
+
+	schema := ParameterSchema{
+		Name:        "param_name",
+		Type:        "string",
+		Description: "string parameter",
+	}
+
+	value := "abc"
+
+	err := schema.ValidateType(value)
+
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+}
+
+// Tests ParameterSchema with type 'boolean'.
+func TestParameterSchemaBoolean(t *testing.T) {
+
+	schema := ParameterSchema{
+		Name:        "param_name",
+		Type:        "boolean",
+		Description: "boolean parameter",
+	}
+
+	value := true
+
+	err := schema.ValidateType(value)
+
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+}
+
+// Tests ParameterSchema with type 'float'.
+func TestParameterSchemaFloat(t *testing.T) {
+
+	schema := ParameterSchema{
+		Name:        "param_name",
+		Type:        "float",
+		Description: "float parameter",
+	}
+
+	t.Run("Test float32 param", func(t *testing.T) {
+		var value float32 = 3.14
+
+		err := schema.ValidateType(value)
+
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	})
+	t.Run("Test float64 param", func(t *testing.T) {
+		value := 3.14
+
+		err := schema.ValidateType(value)
+
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	})
+
+}
+
+// Tests ParameterSchema with type 'array'.
+func TestParameterSchemaStringArray(t *testing.T) {
+
+	itemSchema := ParameterSchema{
+		Name:        "item",
+		Type:        "string",
+		Description: "item of the array",
+	}
+
+	paramSchema := ParameterSchema{
+		Name:        "param_name",
+		Type:        "array",
+		Description: "array parameter",
+		Items:       &itemSchema,
+	}
+
+	value := []string{"abc", "def"}
+
+	err := paramSchema.ValidateType(value)
+
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+}
+
+// Tests ParameterSchema with an undefined type.
+func TestParameterSchemaUndefinedType(t *testing.T) {
+
+	paramSchema := ParameterSchema{
+		Name:        "param_name",
+		Type:        "time",
+		Description: "time parameter",
+	}
+
+	value := time.Now()
+
+	err := paramSchema.ValidateType(value)
+
+	if err == nil {
+		t.Fatal("Expected an error, but got nil")
+	}
+
+}
+
+func TestOptionalStringParameter(t *testing.T) {
+	schema := ParameterSchema{
+		Name:        "nickname",
+		Type:        "string",
+		Description: "An optional nickname",
+		Required:    false, // Explicitly optional
+	}
+
+	t.Run("allows nil value for optional parameter", func(t *testing.T) {
+		err := schema.ValidateType(nil)
+		if err != nil {
+			t.Errorf("ValidateType() with nil should not return an error for an optional parameter, but got: %v", err)
+		}
+	})
+
+	t.Run("allows valid string value", func(t *testing.T) {
+		err := schema.ValidateType("my-name")
+		if err != nil {
+			t.Errorf("ValidateType() should not return an error for a valid string, but got: %v", err)
+		}
+	})
+}
+
+func TestRequiredParameter(t *testing.T) {
+	schema := ParameterSchema{
+		Name:        "id",
+		Type:        "integer",
+		Description: "A required ID",
+		Required:    true, // Explicitly required
+	}
+
+	t.Run("rejects nil value for required parameter", func(t *testing.T) {
+		err := schema.ValidateType(nil)
+		if err == nil {
+			t.Errorf("ValidateType() with nil should return an error for a required parameter, but it didn't")
+		}
+	})
+
+	t.Run("allows valid integer value", func(t *testing.T) {
+		err := schema.ValidateType(12345)
+		if err != nil {
+			t.Errorf("ValidateType() should not return an error for a valid integer, but got: %v", err)
+		}
+	})
+}
+
+func TestOptionalArrayParameter(t *testing.T) {
+	schema := ParameterSchema{
+		Name:        "optional_scores",
+		Type:        "array",
+		Description: "An optional list of scores",
+		Required:    false,
+		Items: &ParameterSchema{
+			Type: "integer",
+		},
+	}
+
+	t.Run("allows nil value for optional array", func(t *testing.T) {
+		err := schema.ValidateType(nil)
+		if err != nil {
+			t.Errorf("ValidateType() with nil should not return an error for an optional array, but got: %v", err)
+		}
+	})
+
+	t.Run("allows valid integer slice", func(t *testing.T) {
+		err := schema.ValidateType([]int{95, 100})
+		if err != nil {
+			t.Errorf("ValidateType() should not return an error for a valid slice, but got: %v", err)
+		}
+	})
+
+	t.Run("rejects slice with wrong item type", func(t *testing.T) {
+		err := schema.ValidateType([]string{"not", "an", "int"})
+		if err == nil {
+			t.Errorf("ValidateType() should have returned an error for a slice with incorrect item types, but it didn't")
+		}
+	})
+}
+
+func TestValidateTypeObject(t *testing.T) {
+	t.Run("generic object allows any value types", func(t *testing.T) {
+		schema := ParameterSchema{
+			Name:                 "metadata",
+			Type:                 "object",
+			AdditionalProperties: true, // or nil
+		}
+
+		// A map with mixed value types should be valid.
+		validInput := map[string]any{
+			"key_string": "a string",
+			"key_int":    123,
+			"key_bool":   true,
+			"key_map":    map[string]any{"id": 1},
+			"key_array":  []string{"id", "number"},
+		}
+		if err := schema.ValidateType(validInput); err != nil {
+			t.Errorf("Expected no error for generic object, but got: %v", err)
+		}
+
+		// A value that is not a map should be invalid.
+		invalidInput := "I am a string, not an object"
+		if err := schema.ValidateType(invalidInput); err == nil {
+			t.Errorf("Expected an error for non-map input, but got nil")
+		}
+	})
+
+	t.Run("typed object validation", func(t *testing.T) {
+		testCases := []struct {
+			name         string
+			valueType    string
+			validInput   map[string]any
+			invalidInput map[string]any
+		}{
+			{
+				name:         "string values",
+				valueType:    "string",
+				validInput:   map[string]any{"header": "application/json"},
+				invalidInput: map[string]any{"bad_header": 123},
+			},
+			{
+				name:         "integer values",
+				valueType:    "integer",
+				validInput:   map[string]any{"user_score": 100},
+				invalidInput: map[string]any{"bad_score": "100"},
+			},
+			{
+				name:         "float values",
+				valueType:    "float",
+				validInput:   map[string]any{"item_price": 99.99},
+				invalidInput: map[string]any{"bad_price": 99},
+			},
+			{
+				name:         "boolean values",
+				valueType:    "boolean",
+				validInput:   map[string]any{"feature_flag": true},
+				invalidInput: map[string]any{"bad_flag": "true"},
+			},
+		}
+
+		for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+				schema := ParameterSchema{
+					Name:                 "test_map",
+					Type:                 "object",
+					AdditionalProperties: &ParameterSchema{Type: tc.valueType},
+				}
+
+				// Test that valid input passes
+				if err := schema.ValidateType(tc.validInput); err != nil {
+					t.Errorf("Expected no error for valid input, got: %v", err)
+				}
+
+				// Test that invalid input fails
+				if err := schema.ValidateType(tc.invalidInput); err == nil {
+					t.Errorf("Expected an error for invalid input, but got nil")
+				}
+			})
+		}
+	})
+
+	t.Run("Fail for object valueType maps", func(t *testing.T) {
+
+		// This schema itself is invalid so there is no valid test case
+		schema := ParameterSchema{
+			Name:                 "test_map",
+			Type:                 "object",
+			AdditionalProperties: &ParameterSchema{Type: "object"},
+		}
+
+		invalidInput := map[string]any{"feature_flag": map[string]any{"id": "123"}}
+		// Test that invalid input fails
+		if err := schema.ValidateType(invalidInput); err == nil {
+			t.Errorf("Expected an error for invalid input, but got nil")
+		}
+	})
+
+	t.Run("Fail for array valueType maps", func(t *testing.T) {
+
+		// This schema itself is invalid so there is no valid test case
+		schema := ParameterSchema{
+			Name:                 "test_map",
+			Type:                 "object",
+			AdditionalProperties: &ParameterSchema{Type: "array"},
+		}
+
+		invalidInput := map[string]any{"feature_flag": []string{"id", "number"}}
+		// Test that invalid input fails
+		if err := schema.ValidateType(invalidInput); err == nil {
+			t.Errorf("Expected an error for invalid input, but got nil")
+		}
+	})
+
+	t.Run("optional and required objects", func(t *testing.T) {
+		// An optional object can be nil
+		optionalSchema := ParameterSchema{Name: "optional_metadata", Type: "object", Required: false}
+		if err := optionalSchema.ValidateType(nil); err != nil {
+			t.Errorf("Expected no error for nil on optional object, but got: %v", err)
+		}
+
+		// A required object cannot be nil
+		requiredSchema := ParameterSchema{Name: "required_metadata", Type: "object", Required: true}
+		if err := requiredSchema.ValidateType(nil); err == nil {
+			t.Error("Expected an error for nil on required object, but got nil")
+		}
+	})
+
+	t.Run("object with unsupported value type in schema", func(t *testing.T) {
+		unsupportedType := "custom_object"
+		schema := ParameterSchema{
+			Name:                 "custom_data",
+			Type:                 "object",
+			AdditionalProperties: &ParameterSchema{Type: unsupportedType},
+		}
+
+		input := map[string]any{"key": "some value"}
+		err := schema.ValidateType(input)
+
+		if err == nil {
+			t.Fatal("Expected an error for unsupported sub-schema type, but got nil")
+		}
+
+		// Check if the error message contains the expected text.
+		expectedErrorMsg := fmt.Sprintf("unknown type '%s'", unsupportedType)
+		if !strings.Contains(err.Error(), expectedErrorMsg) {
+			t.Errorf("Expected error to contain '%s', but got '%v'", expectedErrorMsg, err)
+		}
+	})
+}
+
+func TestParameterSchema_ValidateDefinition(t *testing.T) {
+	t.Run("should succeed for simple valid types", func(t *testing.T) {
+		testCases := []struct {
+			name   string
+			schema *ParameterSchema
+		}{
+			{"String", &ParameterSchema{Name: "p_string", Type: "string"}},
+			{"Integer", &ParameterSchema{Name: "p_int", Type: "integer"}},
+			{"Float", &ParameterSchema{Name: "p_float", Type: "float"}},
+			{"Boolean", &ParameterSchema{Name: "p_bool", Type: "boolean"}},
+		}
+
+		for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+				if err := tc.schema.ValidateDefinition(); err != nil {
+					t.Errorf("expected no error, but got: %v", err)
+				}
+			})
+		}
+	})
+
+	t.Run("should succeed for a valid array schema", func(t *testing.T) {
+		schema := &ParameterSchema{
+			Name:  "p_array",
+			Type:  "array",
+			Items: &ParameterSchema{Type: "string"},
+		}
+		if err := schema.ValidateDefinition(); err != nil {
+			t.Errorf("expected no error for valid array, but got: %v", err)
+		}
+	})
+
+	t.Run("should succeed for valid object schemas", func(t *testing.T) {
+		testCases := []struct {
+			name   string
+			schema *ParameterSchema
+		}{
+			{
+				"Typed Object",
+				&ParameterSchema{
+					Name:                 "p_obj_typed",
+					Type:                 "object",
+					AdditionalProperties: &ParameterSchema{Type: "integer"},
+				},
+			},
+			{
+				"Generic Object (bool)",
+				&ParameterSchema{
+					Name:                 "p_obj_bool",
+					Type:                 "object",
+					AdditionalProperties: true,
+				},
+			},
+		}
+
+		for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+				if err := tc.schema.ValidateDefinition(); err != nil {
+					t.Errorf("expected no error, but got: %v", err)
+				}
+			})
+		}
+	})
+
+	t.Run("should fail when type is missing", func(t *testing.T) {
+		schema := &ParameterSchema{Name: "p_missing_type", Type: "object", AdditionalProperties: &ParameterSchema{Type: ""}}
+		err := schema.ValidateDefinition()
+		if err == nil {
+			t.Fatal("expected an error for missing type, but got nil")
+		}
+		if !strings.Contains(err.Error(), "type is missing") {
+			t.Errorf("error message should mention 'type is missing', but was: %s", err)
+		}
+	})
+
+	t.Run("should fail when type is unknown", func(t *testing.T) {
+		schema := &ParameterSchema{Name: "p_unknown", Type: "object", AdditionalProperties: &ParameterSchema{Type: "some-custom-type"}}
+		err := schema.ValidateDefinition()
+		if err == nil {
+			t.Fatal("expected an error for unknown type, but got nil")
+		}
+		if !strings.Contains(err.Error(), "unknown schema type") {
+			t.Errorf("error message should mention 'unknown schema type', but was: %s", err)
+		}
+	})
+
+	t.Run("should fail for array with missing items property", func(t *testing.T) {
+		schema := &ParameterSchema{Name: "p_bad_array", Type: "array", Items: nil}
+		err := schema.ValidateDefinition()
+		if err == nil {
+			t.Fatal("expected an error for array with nil items, but got nil")
+		}
+		if !strings.Contains(err.Error(), "missing item type definition") {
+			t.Errorf("error message should mention 'missing item type definition', but was: %s", err)
+		}
+	})
+
+	t.Run("should fail for object with invalid AdditionalProperties type", func(t *testing.T) {
+		schema := &ParameterSchema{
+			Name:                 "p_bad_object",
+			Type:                 "object",
+			AdditionalProperties: "a-string-is-not-valid",
+		}
+		err := schema.ValidateDefinition()
+		if err == nil {
+			t.Fatal("expected an error for invalid AdditionalProperties, but got nil")
+		}
+		if !strings.Contains(err.Error(), "must be a boolean or a schema") {
+			t.Errorf("error message should mention 'must be a boolean or a schema', but was: %s", err)
+		}
+	})
+}
